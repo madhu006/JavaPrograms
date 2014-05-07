e3ab256db83427bc5fdece1ba65bb15fa66e322b
madhu006 <madhulikaprasad6@gmail.com>
2014-05-07 17:16:12 +0530

    Added Comments

--- src/HashMapDemo.java
+++ src/HashMapDemo.java
@@ -2,47 +2,43 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
-/**
- * 
- */
 
 /**
- * @author rakesh
+ * @author Madhulika
+ * 
+ * This program shows features of HashMap vs HashTable
  *
  */
 public class HashMapDemo {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		// Created a HashMap
 		Map<String,Integer> hm = new HashMap<String,Integer>();
 		hm.put("Madhulika", 5);
+		// Add key is null and value is null
 		hm.put(null, null);
 		hm.put("stat", null);
 		hm.put("prasad", null);
 		hm.put("math", 9);
-		
+		// Display HashMap
 		System.out.println(hm.get("Madhulika"));
 		System.out.println(hm.get(null));
 		System.out.println(hm.get("stat"));
 		System.out.println(hm.get("prasad"));
 		
+		// Created a HashTable
 		Map<String,Integer> ht = new Hashtable<String,Integer>();
 		ht.put("madhu", 5);
-		
+		//HashTable does not allow key = null and value = null
 		//ht.put(null, null);
-		
-	//	ht.put("prasad", null);
+		// HashTable does not allow value = null
+		//	ht.put("prasad", null);
 		System.out.println(ht.get("madhu"));
-	//	System.out.println(ht.get(null));
-	//	System.out.println(ht.get("prasad"));
+		//	System.out.println(ht.get(null));
+		//	System.out.println(ht.get("prasad"));
 		
-		
-		
-
 	}
 
 }
